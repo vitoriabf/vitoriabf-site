f27a5bf6b852869380ce3772c159a92eca0c9286
Vitoria Fontoura <vitoriabfontoura@gmail.com>
2022-02-06 16:35:56 -0300

    Add files via upload

--- src/Components/SectionHome/index.tsx
+++ src/Components/SectionHome/index.tsx
@@ -14,10 +14,10 @@ export function SectionHome() {
             </p>
 
             <div className="links">
-                <a href="https://www.linkedin.com/in/vitoriabf" target="_blank"><img src="https://img.icons8.com/nolan/50/linkedin-circled.png"/></a>
-                <a href="https://www.twitter.com/vivsbf" target="_blank"><img src="https://img.icons8.com/nolan/50/twitter.png"/></a>
-                <a href="https://www.github.com/vitoriabf" target="_blank"><img src="https://img.icons8.com/nolan/50/github.png"/></a>
-                <a href="mailto:vitoriabfontoura@gmail.com" target="_blank"><img src="https://img.icons8.com/nolan/50/gmail.png"/></a>
+                <a href="https://www.linkedin.com/in/vitoriabf" target="_blank" rel="nofollow, noreferrer, noopener, external"><img src="https://img.icons8.com/nolan/50/linkedin-circled.png" alt="linkedin"/></a>
+                <a href="https://www.twitter.com/vivsbf" target="_blank" rel="nofollow, noreferrer, noopener, external"><img src="https://img.icons8.com/nolan/50/twitter.png" alt="twitter"/></a>
+                <a href="https://www.github.com/vitoriabf" target="_blank" rel="nofollow, noreferrer, noopener, external"><img src="https://img.icons8.com/nolan/50/github.png" alt="github"/></a>
+                <a href="mailto:vitoriabfontoura@gmail.com" target="_blank" rel="nofollow, noreferrer, noopener, external"><img src="https://img.icons8.com/nolan/50/gmail.png" alt="gmail"/></a>
             </div>
 
 
